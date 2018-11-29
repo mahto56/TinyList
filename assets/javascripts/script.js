4b350535aa5f6b7ab27cadcba9a94f3d7e26fae9
Arvind Mahto <mahto.arvin@gmail.com>
2018-11-29 21:27:17 +0000

    tt

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,7 +1,54 @@
-btn = document.getElementById('addbtn')
-btn.addEventListener("click",()=>{
-    // let name= prompt("movie name> ");
-    // console.log(name);
+var grid = new Muuri('.main', {
+  dragEnabled: true,
+  layout: {
+    fillGaps: true
+  }
 });
 
-console.log(data);
+window.addEventListener('load', function () {
+  grid.refreshItems().layout();
+});
+
+
+let editables = document.getElementsByClassName("editable");
+
+
+Array.from(editables).forEach(e => {
+    e.addEventListener("input", function() {
+    console.log("input event fired");
+    grid.refreshItems().layout();
+    }, false);
+});
+
+
+function pintoggle(id){
+    let pin_white = document.getElementById(id+'-pin_white');
+    let pin_black = document.getElementById(id+'-pin_black');
+    
+    let toggle = pin_white.style.display != "none";
+    if(toggle){
+        pin_white.style.display = "none";
+        pin_black.style.display = "";
+    }else{
+        pin_white.style.display = "";
+        pin_black.style.display = "none";
+    }
+    
+    toggle = !toggle;
+}
+
+ function getRandomColor() {
+                var letters = 'BCDEF'.split('');
+                var color = '#';
+                for (var i = 0; i < 6; i++ ) {
+                    color += letters[Math.floor(Math.random() * letters.length)];
+                }
+                return color;
+  }
+  
+let cards = document.getElementsByClassName("card");
+
+Array.from(cards).forEach(e => {
+    e.style.background = randomColor({luminosity: 'light'});
+    e.style.borderColor = e.style.background;
+});
--- index.php
+++ index.php
@@ -14,6 +14,17 @@
   $count = $db->getPendingTasksCount()->fetch_assoc();
 ?>
 
+<style>
+  #container{
+    display:block;
+  }
+  
+</style>
+
+<script src="https://unpkg.com/web-animations-js@2.3.1/web-animations.min.js"></script>
+<script src="https://unpkg.com/hammerjs@2.0.8/hammer.min.js"></script>
+<script src="https://unpkg.com/muuri@0.7.1/dist/muuri.min.js"></script>
+
 <script type="text/javascript">
   function getRandomColor() {
                 var letters = 'BCDEF'.split('');
@@ -49,9 +60,13 @@
   <?php if($tasklists) :?>
     <?php while($list = $tasklists->fetch_assoc()) :?>
       <div class="card" >
+          <div class="card_item">
           <div  class="card__header">
-            <h7 title="Double click to edit" contentEditable="true" onclick="done();" class="task-title" id="<?=$list['task_id'];?>" done="0"><?=$list['task_name'];?></h7>
+            <h7 title="Double click to edit" ondblclick="this.contentEditable=true;this.focus()" onblur="this.contentEditable=false;" onclick="done();" class="task-title editable" id="<?=$list['task_id'];?>" done="0"><?=$list['task_name'];?></h7>
             <div class="card-ctrl">
+              <!--<img src="assets/images/pin-white.png" alt="logo" id="<?=$list['task_id'];?>-pin_white" onclick="pintoggle('<?=$list['task_id'];?>')"/>-->
+              <!--<img src="assets/images/pin-black.png" style="display:none" alt="logo" id="<?=$list['task_id'];?>-pin_black" onclick="pintoggle('<?=$list['task_id'];?>')"/>-->
+
               <img src="assets/images/edit.png" alt="logo" />
               <img src="assets/images/bin.png" alt="logo" /></div>
           </div>
@@ -60,7 +75,7 @@
                 <img src="assets/images/calendar-check.png" alt="logo" />
               
               <span class="date2">Mon, Apr 30</span></p>
-            <p title="Double click to edit" ondblclick="this.contentEditable=true;this.focus()" onblur="this.contentEditable=false;" class="desc">
+            <p title="Double click to edit" ondblclick="this.contentEditable=true;this.focus()"  onblur="this.contentEditable=false;" class="desc editable">
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
             sed diam nonummy nibh euismod tincidunt ut laoreet dolore
             </p>
@@ -79,23 +94,53 @@
           <div class="tags">
           <p class="tag">#shopping</p>
           </div>
+          </div>
       </div>
     <?php endwhile ?>
   <?php  else :?>
     <p>There are no list :(</p>
   <?php endif; ?>
-    
+    <div class="card" >
+          <div class="card_item">
+          <div  class="card__header">
+            <h7 title="Double click to edit" ondblclick="this.contentEditable=true;this.focus()" onblur="this.contentEditable=false;" contentEditable="true" onclick="done();" class="task-title" id="<?=$list['task_id'];?>" done="0">Tmp Task</h7>
+            <div class="card-ctrl">
+              <img src="assets/images/edit.png" alt="logo" />
+              <img src="assets/images/bin.png" alt="logo" /></div>
+          </div>
+          <div>
+            <p class="date">
+                <img src="assets/images/calendar-check.png" alt="logo" />
+              
+              <span class="date2">Mon, Apr 30</span></p>
+            <p title="Double click to edit" ondblclick="this.contentEditable=true;this.focus()" onblur="this.contentEditable=false;" class="desc">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            </p>
+            <!--<div class="subtask">-->
+            <!--  <div><img src="assets/images/right-arrow.png" alt="logo" /></div>-->
+            <!--  <ul>-->
+            <!--    <li>jdjkd</li>-->
+            <!--    <li>dhdhjd</li>-->
+            <!--    <li>jdjd</li>-->
+            <!--    <li>sds</li>-->
+            <!--    <li>dsds</li>-->
+            <!--  </ul>-->
+              
+            <!--</div>-->
+          </div>
+          <div class="tags">
+          <p class="tag">#shopping</p>
+          </div>
+          </div>
+      </div>
   </div>
 </div>         
-
-<!--<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>-->
-<!--<script>-->
-<!--  var elem = document.querySelector('.main');-->
-<!--  var msnry = new Masonry( elem, {-->
-<!--  itemSelector: '.card',-->
-<!--  columnWidth: 200-->
-<!--  });-->
-  
-
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/randomcolor/0.5.2/randomColor.min.js"></script>
+<script type="text/javascript" src="assets/javascripts/script.js"></script>
 <!--</script>-->
 <?php include "include/footer.php"; ?>
